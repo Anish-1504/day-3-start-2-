90e5f1866b3559f1c3331279344e52313f923a69
Anish Patil <123080347+Anish-1504@users.noreply.github.com>
2023-02-04 18:20:13 +0000

    add some few lines

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ height = int(input("What is your height in cm? "))
 if height>=120:
  print("you can ride the roller coster")
  age = int(input("Enter ur Age"))
- if age<= 18:
-  print("plzz pay 7$")
+ if age<12:
+   print("pay 5$")
+ elif age<= 18:
+   print("plzz pay 7$")
  else:
   print("plzz pay 12$")
 else:
